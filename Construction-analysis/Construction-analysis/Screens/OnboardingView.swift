6b82780aa902e21b021f0ce9e543deedbcf1ef12
Carlos_A_Amaral <carlos.a.amaral@sesisc.org.br>
2022-05-06 00:36:48 -0300

    Ajustes

--- Construction-analysis/Construction-analysis/Screens/OnboardingView.swift
+++ Construction-analysis/Construction-analysis/Screens/OnboardingView.swift
@@ -14,7 +14,7 @@ struct OnboardingView: View {
             TabView {
                 OnboardingPageComponent(isLast: false, imageName: "worker_building", mainText: "Valide sua segurança" ,subText: "Na plataforma você pode validar o nível de segurança do seu local de obra")
                 OnboardingPageComponent(isLast: false, imageName: "worker_group_planning", mainText: "Outro Valide sua segurança" ,subText: "Na plataforma você pode validar o nível de segurança do seu local de obra")
-                OnboardingPageComponent(isLast: true, imageName: "logo_cis_colorida", mainText: "Olá!", subText: "Por: Centro de Inovação SESI para tecnologias para saúde")
+                OnboardingPageComponent(isLast: true, imageName: "logo_cis_colorida", mainText: "Olá!", subText: "Por: Carlos Alberto Barcelos do Amaral")
             }
             .tabViewStyle(.page(indexDisplayMode: .never))
         }
